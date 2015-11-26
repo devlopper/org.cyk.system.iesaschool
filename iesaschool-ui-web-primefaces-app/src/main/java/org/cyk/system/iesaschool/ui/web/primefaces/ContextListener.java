112a7d81fb847bbeae41697741e25d42ff16cd8f
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2015-11-26 19:19:24 +0000

    Change-Id: I9d8f9e02dc96aea107c1dc0fbc2f91fb774daf1f

--- iesaschool-ui-web-primefaces-app/src/main/java/org/cyk/system/iesaschool/ui/web/primefaces/ContextListener.java
+++ iesaschool-ui-web-primefaces-app/src/main/java/org/cyk/system/iesaschool/ui/web/primefaces/ContextListener.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.annotation.WebListener;
 
+import org.cyk.system.root.business.api.Crud;
 import org.cyk.system.root.model.party.person.AbstractActor;
 import org.cyk.system.root.ui.web.primefaces.api.RootWebManager;
 import org.cyk.system.school.model.actor.Student;
@@ -18,8 +19,13 @@ import org.cyk.ui.api.command.menu.AbstractMenu;
 import org.cyk.ui.api.command.menu.MenuManager;
 import org.cyk.ui.api.command.menu.MenuManager.ModuleGroup;
 import org.cyk.ui.api.command.menu.UIMenu;
+import org.cyk.ui.api.data.collector.form.FormConfiguration;
+import org.cyk.ui.api.model.party.DefaultActorEditFormModel;
+import org.cyk.ui.api.model.party.DefaultActorReadFormModel;
 import org.cyk.ui.api.model.party.DefaultPersonEditFormModel;
 import org.cyk.ui.web.primefaces.data.collector.control.ControlSetAdapter;
+import org.cyk.ui.web.primefaces.page.BusinessEntityFormManyPageListener;
+import org.cyk.ui.web.primefaces.page.BusinessEntityFormOnePageListener;
 import org.cyk.ui.web.primefaces.page.crud.AbstractActorConsultPage;
 import org.cyk.ui.web.primefaces.page.crud.AbstractActorConsultPage.MainDetails;
 import org.cyk.ui.web.primefaces.page.tools.AbstractActorConsultPageAdapter;
@@ -58,6 +64,40 @@ public class ContextListener extends AbstractSchoolContextListener implements Se
 		super.sessionContextualMenuCreated(userSession, menu);
 		menu.remove(MenuManager.COMMANDABLE_EVENT_CALENDAR_IDENTIFIER);
 		menu.remove(MenuManager.COMMANDABLE_NOTIFICATIONS_IDENTIFIER);
+	}
+	
+	@Override
+	protected <ACTOR extends AbstractActor> void registerBusinessEntityFormOnePageListener(Class<ACTOR> actorClass,BusinessEntityFormOnePageListener<?> listener) {
+		super.registerBusinessEntityFormOnePageListener(actorClass, listener);
+		if(actorClass.equals(Teacher.class)){
+			listener.getFormConfigurationMap().get(Crud.CREATE).get(FormConfiguration.TYPE_INPUT_SET_SMALLEST).addRequiredFieldNames(DefaultActorEditFormModel.FIELD_REGISTRATION_CODE);
+			listener.getFormConfigurationMap().get(Crud.CREATE).get(FormConfiguration.TYPE_INPUT_SET_SMALLEST).addFieldNames(DefaultPersonEditFormModel.FIELD_TITLE
+					,DefaultPersonEditFormModel.FIELD_SIGNATURE_SPECIMEN);
+			
+			listener.getFormConfigurationMap().get(Crud.UPDATE).get(DefaultPersonEditFormModel.TAB_PERSON_ID).addFieldNames(
+					DefaultActorEditFormModel.FIELD_REGISTRATION_CODE,DefaultPersonEditFormModel.FIELD_TITLE);
+			
+		}else if(actorClass.equals(Student.class)){
+			listener.getFormConfigurationMap().get(Crud.CREATE).get(FormConfiguration.TYPE_INPUT_SET_SMALLEST).addRequiredFieldNames(DefaultActorEditFormModel.FIELD_REGISTRATION_CODE);
+			listener.getFormConfigurationMap().get(Crud.CREATE).get(FormConfiguration.TYPE_INPUT_SET_SMALLEST).addFieldNames(DefaultPersonEditFormModel.FIELD_SURNAME
+					,DefaultPersonEditFormModel.FIELD_BIRTH_DATE,DefaultPersonEditFormModel.FIELD_BIRTH_LOCATION
+					,DefaultPersonEditFormModel.FIELD_SEX,DefaultPersonEditFormModel.FIELD_IMAGE);
+			
+			listener.getFormConfigurationMap().get(Crud.UPDATE).get(DefaultPersonEditFormModel.TAB_PERSON_ID).addFieldNames(
+					DefaultActorEditFormModel.FIELD_REGISTRATION_CODE,DefaultPersonEditFormModel.FIELD_SURNAME
+					,DefaultPersonEditFormModel.FIELD_BIRTH_DATE,DefaultPersonEditFormModel.FIELD_BIRTH_LOCATION
+					,DefaultPersonEditFormModel.FIELD_SEX,DefaultPersonEditFormModel.FIELD_IMAGE);
+		}
+	}
+	
+	@Override
+	protected <ACTOR extends AbstractActor> void registerBusinessEntityFormManyPageListener(Class<ACTOR> actorClass,BusinessEntityFormManyPageListener<?> listener) {
+		if(actorClass.equals(Teacher.class)){
+			listener.getFormConfigurationMap().get(Crud.READ).get(FormConfiguration.TYPE_INPUT_SET_SMALLEST).addRequiredFieldNames(DefaultActorReadFormModel.FIELD_REGISTRATION_CODE);
+		}else if(actorClass.equals(Student.class)){
+			listener.getFormConfigurationMap().get(Crud.READ).get(FormConfiguration.TYPE_INPUT_SET_SMALLEST).addRequiredFieldNames(DefaultActorReadFormModel.FIELD_REGISTRATION_CODE);
+		}
+		super.registerBusinessEntityFormManyPageListener(actorClass, listener);
 	}
 	
 	/**/

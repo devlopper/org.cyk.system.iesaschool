7b76d78be6ff3b57099966541c498be53dd91c58
devlopper <kycdev@gmail.com>
2016-11-01 12:36:57 +0000

    Change-Id: Ifc39fe79072bddb6d202f44113d8b514339618f0

--- iesaschool-ui-web-primefaces-app/src/main/java/org/cyk/system/iesaschool/ui/web/primefaces/SystemMenuBuilder.java
+++ iesaschool-ui-web-primefaces-app/src/main/java/org/cyk/system/iesaschool/ui/web/primefaces/SystemMenuBuilder.java
@@ -7,27 +7,6 @@ public class SystemMenuBuilder extends org.cyk.system.school.ui.web.primefaces.a
 	private static final long serialVersionUID = 6995162040038809581L;
 
 	private static SystemMenuBuilder INSTANCE;
-	/*
-	@Override
-	public SystemMenu build(UserSession userSession) {
-		SystemMenu systemMenu = new SystemMenu();
-		if(userSession.hasRole(Role.MANAGER)){
-			addBusinessMenu(userSession,systemMenu,getStudentCommandable(userSession, null));
-			addBusinessMenu(userSession,systemMenu,getEmployeeCommandable(userSession, null));
-		}
-		//addBusinessMenu(userSession,systemMenu,getFinanceCommandable(userSession, null));
-		//addBusinessMenu(userSession,systemMenu,getAcademicCommandable(userSession, null));
-		//addBusinessMenu(userSession,systemMenu,getServiceCommandable(userSession, null));
-		//addBusinessMenu(userSession,systemMenu,getMessageCommandable(userSession, null));
-		
-		addBusinessMenu(userSession,systemMenu,getRegularActivitiesCommandable(userSession, null));
-		addBusinessMenu(userSession,systemMenu,getResultsCardCommandable(userSession, null));
-		
-		addReferences(userSession, systemMenu, null);
-		
-		initialiseNavigatorTree(userSession);//TODO make it as a call after .build
-		return systemMenu;
-	}*/
 	
 	public static SystemMenuBuilder getInstance(){
 		if(INSTANCE==null)
